80ae7e9b7b265d50176d37c3e40739a745d871ec
ChristianF22 <christianfonseca991@gmail.com>
2026-02-05 22:22:44 -0300

    Portfolio 1.0

--- Script/script.js
+++ Script/script.js
@@ -1,23 +1,78 @@
-function nextNav(type){
+function nextNav(type) {
     const select = document.querySelector(".select");
+    const home = document.querySelector("#home")
+    const about = document.querySelector("#about")
+    const exp = document.querySelector("#exp")
+    const contact = document.querySelector("#contact")
 
-    switch (type){
+
+    switch (type) {
         case 1:
-            select.style.left = "2.7rem";
+            select.style.left = "-1.3rem";
             select.style.width = "5rem";
+
+            home.style.display = "flex"
+            about.style.display = "none"
+            exp.style.display = "none"
+            contact.style.display = "none"
+
+            setTimeout(() => {
+                home.style.opacity = "100%"
+                about.style.opacity = "0%"
+                exp.style.opacity = "0%"
+                contact.style.opacity = "0%"
+            }, 1);
             break;
         case 2:
-            select.style.left = "7.2rem";
-            select.style.width = "8rem";
+            select.style.left = "3.6rem";
+            select.style.width = "7rem";
+
+            home.style.display = "none"
+            about.style.display = "flex"
+            exp.style.display = "none"
+            contact.style.display = "none"
+
+            setTimeout(() => {
+                home.style.opacity = "0%"
+                about.style.opacity = "100%"
+                exp.style.opacity = "0%"
+                contact.style.opacity = "0%"
+            }, 1)
+
             break;
         case 3:
-            select.style.left = "14.6rem";
+            select.style.left = "10.5rem";
             select.style.width = "8rem";
-          break;
+
+            home.style.display = "none"
+            about.style.display = "none"
+            exp.style.display = "flex"
+            contact.style.display = "none"
+
+            setTimeout(() => {
+                home.style.opacity = "0%"
+                about.style.opacity = "0%"
+                exp.style.opacity = "100%"
+                contact.style.opacity = "0%"
+            }, 1)
+
+            break;
         case 4:
-            select.style.left = "22rem";
+            select.style.left = "18rem";
             select.style.width = "7rem";
-            break;  
-          
+
+            home.style.display = "none"
+            about.style.display = "none"
+            exp.style.display = "none"
+            contact.style.display = "flex"
+
+            setTimeout(() => {
+                home.style.opacity = "0%"
+                about.style.opacity = "0%"
+                exp.style.opacity = "0%"
+                contact.style.opacity = "100%"
+            }, 1)
+
+            break;
     }
 }
